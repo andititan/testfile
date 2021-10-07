02e0d1e263eafad3b1e912a884b3916be83900a8
Andi <pirinkovandi@gmail.com>
2021-10-07 14:51:04 +0300

    Implemented fibonacci

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Hello, world3!\n";
+    int n;
+    cout << "n";
+    cin >> n;
+    int num1 = 0, num2 = 1, sum = 0;
+
+    for(int i = 0; i <= n; i++){
+
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+
+        cout << sum << ", ";
+    }
+return 0;
+
 }
