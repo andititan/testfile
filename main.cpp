0108461b981db225913afadfa59d160ecd36c31b
Andi <pirinkovandi@gmail.com>
2021-10-07 16:19:30 +0300

    fibonacci changes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cout << "n";
-    cin >> n;
-    int num1 = 0, num2 = 1, sum = 0;
+int fibonacci(void){
 
-    for(int i = 0; i <= n; i++){
+        int n;
+        cout << "n\n";
+        cin >> n;
 
+        int num1 = 0, num2 = 1, sum = 0;
+
+        for(int i = 0; i <= n; i++){
+        
         sum = num1 + num2;
         num1 = num2;
         num2 = sum;
-
-        cout << sum << ", ";
     }
-return 0;
+    cout << sum << endl ;
+    }
 
+int main(){
+
+    fibonacci();
+
+return 0;
 }
