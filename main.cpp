4c14a1cfdc330e8d07a6c84efdfcb62bd44ce84c
Andi <pirinkovandi@gmail.com>
2021-10-08 15:26:21 +0300

    text file array

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int fibonacci(void){
-
-        int n;
-        cout << "n\n";
-        cin >> n;
-
-        int num1 = 0, num2 = 1, sum = 0;
-
-        for(int i = 0; i <= n; i++){
-        
-        sum = num1 + num2;
-        num1 = num2;
-        num2 = sum;
-    }
-    cout << sum << endl ;
-    }
-
 int main(){
 
-    fibonacci();
+    int n = 0;
+    int sum = 0;
+
+    FILE *fptr = fopen("newprogram.txt","r");
+
+    fscanf(fptr, "%d,", &n);
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++){
+        fscanf(fptr, "%d,", &arr[i] );
+        sum+=arr[i];
+    }
+
+    cout << sum;
+
+    fclose(fptr);
+    delete [] arr;
 
 return 0;
 }
