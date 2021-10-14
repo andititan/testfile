42b15bcedbb556ccc78b680514d1ed1d8789a2cb
Andi <pirinkovandi@gmail.com>
2021-10-14 11:38:17 +0300

    read lines file

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@ int main(){
 
     int n = 0;
     int sum = 0;
+    int fNum = 0;
+    int sNum = 0;
 
     FILE *fptr = fopen("newprogram.txt","r");
 
     fscanf(fptr, "%d,", &n);
-    int *arr = new int[n];
+    //int *arr = new int[n];
 
     for (int i = 0; i < n; i++){
-        fscanf(fptr, "%d,", &arr[i] );
-        sum+=arr[i];
+        fscanf(fptr, "%d %d,", &fNum, &sNum);
+        //fscanf(fptr, "%d,", &sNum);
+        sum = fNum + sNum;
+        cout << sum << endl;
     }
 
-    cout << sum;
-
     fclose(fptr);
-    delete [] arr;
+    //delete [] arr;
 
 return 0;
 }
