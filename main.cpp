58b21ee5849d7cd0386bc825dee9b92a775713ce
Andi <pirinkovandi@gmail.com>
2021-10-14 14:50:14 +0300

    N and M, file work

--- main.cpp
+++ main.cpp
@@ -4,24 +4,31 @@ using namespace std;
 int main(){
 
     int n = 0;
-    int sum = 0;
-    int fNum = 0;
-    int sNum = 0;
+    int m = 0;
+    
+    int a = 0;
+
+    double sum = 0;
+    double avg = 0;
 
     FILE *fptr = fopen("newprogram.txt","r");
 
     fscanf(fptr, "%d,", &n);
-    //int *arr = new int[n];
 
     for (int i = 0; i < n; i++){
-        fscanf(fptr, "%d %d,", &fNum, &sNum);
-        //fscanf(fptr, "%d,", &sNum);
-        sum = fNum + sNum;
-        cout << sum << endl;
+        
+        fscanf(fptr, "%d,", &m);
+        for(int y = 0; y < m; y++){
+            fscanf(fptr, "%d,", &a);
+            sum += a;
+
+        }
+        avg = sum / m;
+        sum = 0;
+        cout << avg << endl;
     }
 
     fclose(fptr);
-    //delete [] arr;
 
 return 0;
 }
