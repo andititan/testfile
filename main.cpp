7a81e4ea6c44dbd63c02d0a0ee6e02beac84b8aa
Andi <pirinkovandi@gmail.com>
2021-10-22 17:20:11 +0300

    Custom getline part 1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-int getStringLength(char*str){
+int getStringLength(char *str){
     int n = 0;
     while(str[n] != '\0'){
         n++;
@@ -10,12 +9,25 @@ int getStringLength(char*str){
     return n;
 }
 
+void getLine(char *str, FILE *file, int size = 0){
+    for(int i = 0; i < size; i++){
+        char ch;
+        ch = fgetc(file);
+
+        if(ch == '\n' || ch == '\0'){
+            break;
+        }else{
+            str[i] = ch;
+        }
+    }
+}
+
 int main(){
     FILE *fptr = fopen("newprogram.txt","r");
 
     char line [256];
     
-    fgets(line, 256, fptr);
+    getLine(line, fptr, 256);
 
     int a = getStringLength(line); 
     int b = a-1;
@@ -27,6 +39,7 @@ int main(){
             line[b] = c;
             b--;
     }
+
     cout << line << endl;
     fclose(fptr);
 
