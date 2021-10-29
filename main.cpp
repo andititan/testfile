9de7abf91f97edb11904ccfda3dbd16ed236feb3
Andi <pirinkovandi@gmail.com>
2021-10-29 14:33:49 +0300

    Switch Date Finale

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(){
     cout << "Enter date: ";
     cin >> date;
 
-    cout << "Enter month: " << endl;
+    cout << "Enter month: ";
     cin >> month;
 
-    cout << "Enter year: " << endl;
+    cout << "Enter year: ";
     cin >> year;
 
     switch(month){
@@ -74,73 +74,82 @@ int main(){
         break;
     }
 
-    if(y > 1699 && y < 1800){
+    if(year > 1699 && year < 1800){
         c = 4;
     }
 
-    if(y > 1799 && y < 1900){
+    if(year > 1799 && year < 1900){
         c = 2;
     }
 
-    if(y > 1899 && y < 2000){
+    if(year > 1899 && year < 2000){
         c = 0;
     }
 
-    if(y > 1999 && y < 2100){
+    if(year > 1999 && year < 2100){
         c = 6;
     }
 
-    if(y > 2099 && y < 2200){
+    if(year > 2099 && year < 2200){
         c = 4;
     }
 
-    if(y > 2199 && y < 2300){
+    if(year > 2199 && year < 2300){
         c = 2;
     }
 
-    if(y > 2299 && y < 2400){
+    if(year > 2299 && year < 2400){
         c = 0;
     }
     
-    int a = 0;
     int b = 0;
     int sum = 0;
 
-    a = y % 100;
-    b = a % 4;
-    sum = a + b;
+    y = year % 100;
+    b = y / 4;
+    sum = y + b;
     y = sum % 7;
 
     x = (y + m + c + date) % 7;
     
+    if(year %4 == 0 && year % 100 != 0 || year % 400 == 0){
+        if(m == 1 || m == 2){
+            x = x - 1;
+        }
+    }
+    
     switch(x){
+        case -1:
+            cout << "Saturday" << endl;
+            break;
+
         case 0:
-        cout << "Sunday" << endl;
-        break;
+            cout << "Sunday" << endl;
+            break;
 
         case 1:
-        cout << "Monday" << endl;
-        break;
+            cout << "Monday" << endl;
+            break;
 
         case 2:
-        cout << "Tuesday" << endl;
-        break;
+            cout << "Tuesday" << endl;
+            break;
 
         case 3:
-        cout << "Wednesday" << endl;
-        break;
+            cout << "Wednesday" << endl;
+            break;
 
         case 4:
-        cout << "Thursday" << endl;
-        break;
+            cout << "Thursday" << endl;
+            break;
 
         case 5:
-        cout << "Friday" << endl;
-        break;
+            cout << "Friday" << endl;
+            break;
 
         case 6:
-        cout << "Saturday" << endl;
-        break;
+            cout << "Saturday" << endl;
+            break;
     }
     return 0;
 }
