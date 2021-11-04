615587f6e83944045a105bf5d7443e914149c8ef
Andi <pirinkovandi@gmail.com>
2021-11-04 18:04:08 +0200

    Trapeca

--- main.cpp
+++ main.cpp
@@ -1,28 +1,62 @@
 #include <iostream>
 using namespace std;
 
+int initialSpaces(int lines, int currentLine){
+	return lines - (currentLine + 1);
+}
+
+void printSpaces(int n) {
+	for(int i = 0; i < n; i++){
+		cout << " ";
+	}	
+}
+
+int firstLine(int n){
+	for(int a = 0; a < initialSpaces(n, 0); a++){
+		cout << " ";
+	}
+
+	for(int i = 0; i < n; i++){
+		cout << "* ";
+	}
+	cout << "\n";
+}
+
+
+int Mid(int n){
+	for(int a = 1; a < n - 1; a ++){
+
+		printSpaces(initialSpaces(n, a));
+		cout << "*";
+		
+		const int spaces2 = a * 2 - 1;
+		printSpaces(spaces2);
+		cout << "*";
+
+		const int spacesMid = 2 * initialSpaces(n, a) - 1;
+		printSpaces(spacesMid);
+		cout << "*";
+
+		printSpaces(spaces2);
+		cout << "*";
+		cout << "\n";
+	}
+}
+
+int Last(int n){
+	for(int i = 0; i < n * 2 - 1; i++){
+		cout << "*" << " ";
+	}
+}
+
 int main(){
     int n = 0;
     cin >> n;
-    for(int line = 1; line <= n; line++){
-        
-		for(int i = 1; i < (n - line + 1); i++){
-			cout << " ";
-		}
-        
-		cout << "/";
-		
-        for(int a = 2; a < line + line; a++){
-			if(line == n){
-				cout << "_";
-			}else{
-				cout << " ";
-			}
-		}
-        
-		cout << "\\";
+    
+    firstLine(n);
 
-		cout << "\n";
-	}
+	Mid(n);
+    
+	Last(n);
     return 0;
 }
