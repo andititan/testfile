977566b054df117509ba9724df52a8fa2bcf695e
Andi <pirinkovandi@gmail.com>
2021-11-04 11:34:42 +0200

    Triangle

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@ using namespace std;
 int main(){
     int n = 0;
     cin >> n;
-    for (int line = 1; line <= n; line++){
-
-		for (int i = 1; i < (n - line + 1); i++){
+    for(int line = 1; line <= n; line++){
+        
+		for(int i = 1; i < (n - line + 1); i++){
 			cout << " ";
 		}
-
-		for (int i = 1; i <= line; i++){
-			cout << "*";
+        
+		cout << "/";
+		
+        for(int a = 2; a < line + line; a++){
+			if(line == n){
+				cout << "_";
+			}else{
+				cout << " ";
+			}
 		}
         
-        cout << " ";
-        for (int i = 1; i <= line; i++){
-			cout << "*";
-		}
+		cout << "\\";
+
 		cout << "\n";
 	}
     return 0;
