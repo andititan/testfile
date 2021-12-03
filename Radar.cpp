32c3d392d3b7fed3226e11a509558dd303a64e7e
Andi <pirinkovandi@gmail.com>
2021-12-03 17:36:39 +0200

    Pochti gotov radar

--- Radar.cpp
+++ Radar.cpp
@@ -1,84 +1,93 @@
 #include <iostream>
-#include <assert.h>
 using namespace std;
 
 int Line(int x, int y, int x1 , int y1, char arr[][201]){
     int newX = x;
     int	newY = y;
-
-	float dx = x1 - x;
-	float dy = y1 - y;
+	float sdx = x1 - x;
+	float sdy = y1 - y;
+	float dx = abs(sdx) + 1;
+	float dy = abs(sdy) + 1;
 	float tgd = dx / dy;
+	if (sdx * sdy < 0) {
+		tgd = -tgd;
+	}
 	float e = 0;
 
-    while(newX != x1 || newY != y1){
-		
+    for(int iter = 0; iter<dy; iter++) {
 		e += tgd;
 
+		float cf = 1;//correction factor
+
 		if(x1 < x && y1 > y){
-			if(e < -0.5){
-				newX--;
-				newY++;
-				e++;
-			}else{
-				newY++;
+			if(abs(e) > 0.5){
+				while(abs(e) > 0.5){
+					arr[newX][newY] = '#';
+					e += cf;
+					newX -= cf;
+				}
 			}
+			newY += cf;
 		}
 
 		if(x1 < x && y1 < y){
-			if(e > 0.5){
-				newX--;
-				newY--;
-				e--;
-			}else{
-				newY--;
+			if(abs(e) > 0.5){
+				while(abs(e) > 0.5){
+					arr[newX][newY] = '#';
+					e -= cf;
+					newX -= cf;
+				}
 			}
+			newY -= cf;
 		}
 
 		if(x1 > x && y1 < y){
-			if(e < -0.5){
-				newX++;
-				newY--;
-				e++;
-			}else{
-				newY--;
+			if(abs(e) < 0.5){
+				while(abs(e) < 0.5){
+					arr[newX][newY] = '#';
+					e += cf;
+					newX += cf;
+				}
 			}
+			newY-= cf;
 		}
 
 		if(x1 > x && y1 > y){
 			if(e > 0.5){
-				newX++;
-				newY++;
-				e--;
-			}else{
-				newY++;
+				while(abs(e) > 0.5){
+					arr[newX][newY] = '#';
+					e -= cf;
+					newX += cf;
+				}
 			}
+			newY += cf;
 		}
 
 		if(x1 < x && y1 == y){
 			newX--;
+			arr[newX][newY] = '#';
 		}
 
 		if(x1 > x && y1 == y){
 			newX++;
+			arr[newX][newY] = '#';
 		}
 
 		if(x1 == x && y1 > y){
 			newY++;
+			arr[newX][newY] = '#';
 		}
 
 		if(x1 == x && y1 < y){
 			newY--;
+			arr[newX][newY] = '#';
 		}
-		// assert(x1 <= newX);
-		// assert(newY <= y1);
-		arr[newX][newY] = '#';
     }
 	return 0;
 }
 
 int main(){
-    int radius = 5;
+    int radius = 8;
     char arr[201][201] = {0};
 
     int rows = (radius * 2) + 1;
@@ -92,20 +101,37 @@ int main(){
     int y = radius;
     int x = radius;
 
-    int x1 = 10;
-    int y1 = 1;
+    int x1 = 0;
+    int y1 = radius;
 
     arr[x][y] = '*';
 
-    //while(true){
+	char input;
+	
+	system("stty raw");
+
+    while(true){
+
+		system("clear");
+
         Line(x, y, x1, y1, arr);
+
         for(int i = 0; i < rows; i ++){
             for(int a = 0; a < rows; a++){
                 printf("%c", arr[i][a]);
             }
-        printf("\n");
+        	printf("\n\r");
         }
-    //}
-    
+
+		printf("Enter e to move");
+		cin >> input;
+
+		if(input == 'e'){
+			x1++;
+		}
+    }
+
+    system("stty cooked");
+
     return 0;
 }
