19839a47ead176fd56ca2659c178455dea83287d
Andi <pirinkovandi@gmail.com>
2021-12-17 17:56:40 +0200

    Radar with adding objects

--- Radar.cpp
+++ Radar.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <unistd.h>
-#include <assert.h>
 
 using namespace std;
 
-int SetArr(char arr[][201], int x, int y, char V){
-	assert(x >= 0 && x < 201);
-	assert(y >= 0 && y < 201);
-	arr[x][y] = V;
+int objectX = -1;
+int objectY = -1;
+
+int SetArr(char arr[][201], int x, int y, char V, bool &isSeen){
+	if(objectX == x && objectY == y){
+		isSeen = true;
+	}else{
+		arr[x][y] = V;
+	}
 }
 
-int AddObject(int x, int y, char arr[][201]){
-	
-}
+// int AddObject(char arr[][201]){
+// 	arr[objectX][objectY] = '$';
+// }
 
-int Line(int x, int y, int x1 , int y1, char arr[][201]){
+int Line(int x, int y, int x1 , int y1, char arr[][201], bool &isSeen){
     int newX = x;
     int	newY = y;
 
@@ -40,86 +44,90 @@ int Line(int x, int y, int x1 , int y1, char arr[][201]){
 		if(x1 < x && y1 > y){
 			if(abs(e) > 0.5){
 				while(abs(e) > 0.5){
-					SetArr(arr, newX, newY, '#');
+					SetArr(arr, newX, newY, '#', isSeen);
 					e += cf;
 					newX -= cf;
 				}
 			}
 			newY += cf;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
 
 		if(x1 < x && y1 < y){
 			if(abs(e) > 0.5){
 				while(abs(e) > 0.5){
-					SetArr(arr, newX, newY, '#');
+					SetArr(arr, newX, newY, '#', isSeen);
 					e -= cf;
 					newX -= cf;
 				}
 			}
 			newY -= cf;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
 
 		if(x1 > x && y1 < y){
 			if(abs(e) > 0.5){
 				while(abs(e) > 0.5){
-					SetArr(arr, newX, newY, '#');
+					SetArr(arr, newX, newY, '#', isSeen);
 					e += cf;
 					newX += cf;
 				}
 			}
 			newY -= cf;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
 
 		if(x1 > x && y1 > y){
 			if(e > 0.5){
 				while(abs(e) > 0.5){
-					SetArr(arr, newX, newY, '#');
+					SetArr(arr, newX, newY, '#', isSeen);
 					e -= cf;
 					newX += cf;
 				}
 			}
 			newY += cf;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
 
 		if(x1 < x && y1 == y){
 			while(newX > x1){
 				newX--;
-				SetArr(arr, newX, newY, '#');
+				SetArr(arr, newX, newY, '#', isSeen);
 			}	
 		}
 
 		if(x1 > x && y1 == y){
 			while(newX < x1){
 				newX++;
-				SetArr(arr, newX, newY, '#');
+				SetArr(arr, newX, newY, '#', isSeen);
 			}	
 		}
 
 		if(x1 == x && y1 > y){
 			newY++;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
 
 		if(x1 == x && y1 < y){
 			newY--;
-			SetArr(arr, newX, newY, '#');
+			SetArr(arr, newX, newY, '#', isSeen);
 		}
     }
 	return 0;
 }
 
-int Print(int rows, int radius, int x, int y, int x1, int y1, char arr[][201]){
+int Print(int rows, int radius, int x, int y, int x1, int y1, char arr[][201], bool &isSeen){
 	for(int i = 0; i < rows; i ++){
         for(int a = 0; a < rows; a++){
             arr[i][a] = ' ';
         }
     }
 
-    Line(x, y, x1, y1, arr);
+    Line(x, y, x1, y1, arr, isSeen);
+
+	if(isSeen == true){
+		arr[objectX][objectY] = '$';
+	}
 
 	for(int i = 0; i < rows; i ++){
 		for(int a = 0; a < rows; a++){
@@ -141,37 +149,59 @@ int main(){
 
     int y = radius;
     int x = radius;
+	
+	bool isSeen = false;
 
     int x1 = 1;
     int y1 = 1;
 
+	char input;
+
+	Print(rows, radius, x, y, x1, y1, arr, isSeen);
+
+	system("stty raw");
+
     while(true){
+		cout << "Enter 'e' to move or 'a' to add: ";
+		cin >> input;
+
+
+		cout << "'\n";
+		if(input == 'a'){
+			cout << "Enter cordinates: ";
+			cin >> objectX;
+			cin >> objectY;
+		}
+		
+		if(input == 'q'){
+			return 0;
+		}
 
 		system("clear");
 
-		while(y1 != rows){
-			y1++;
-			Print(rows, radius, x, y, x1, y1, arr);
-			usleep(100000);
-		}
+		if(input == 'e'){
+			if(y1 != rows - 1 && x1 == 1){
+				y1++;
+				Print(rows, radius, x, y, x1, y1, arr, isSeen);
+			}
 
-		while(x1 != rows){
-			x1++;
-			Print(rows, radius, x, y, x1, y1, arr);
-			usleep(100000);
-		}
+			else if(x1 != rows - 1 && y1 > 1){
+				x1++;
+				Print(rows, radius, x, y, x1, y1, arr, isSeen);
+			}
 
-		while(y1 > 1){
-			y1--;
-			Print(rows, radius, x, y, x1, y1, arr);
-			usleep(100000);
-		}
+			else if(y1 > 1 && x1 == rows - 1){
+				y1--;
+				Print(rows, radius, x, y, x1, y1, arr, isSeen);
+			}
 
-		while(x1 > 1){
-			x1--;
-			Print(rows, radius, x, y, x1, y1, arr);
-			usleep(100000);
-		}
+			else if(x1 > 1 && y1 == 1){
+				x1--;
+				Print(rows, radius, x, y, x1, y1, arr, isSeen);
+			}
+		}	
     }
+	system("stty cooked");
+
     return 0;
 }
